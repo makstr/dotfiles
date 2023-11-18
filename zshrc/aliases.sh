3ab022b26ab5cef47477272fdd78e9b50cd462b0
Ubuntu <max.trawinski@gmail.com>
2023-11-18 23:55:33 +0000

    feat: add zshrc aliases

---
+++ zshrc/aliases.sh
@@ -0,0 +1,41 @@
+alias zrl="source ~/.zshrc"
+alias tmx="tmux new-session -A -s ADMIN"
+alias tmxkill="tmux kill-server"
+alias j="just"
+
+alias lzg="lazygit"
+alias gt="git log --graph --oneline --all"
+alias gtr="git log --graph --pretty=medium --all"
+alias gdss="git diff --shortstat"
+
+if docker info >/dev/null 2>&1; then
+	alias drc="docker-compose"
+	alias drcrun="docker-compose run --rm"
+	alias dexec="docker exec -it"
+
+	docker_stop_all() {
+		docker stop $(docker ps -a -q)
+	}
+
+	docker_kill_all() {
+		docker kill $(docker ps -q)
+	}
+
+fi
+
+# obtain ip address
+
+alias ip='ifconfig | grep "^\tinet\ [0-9\.]*\ .*\ b" -i -m 1 | sed -E "s/[^0-9\.]+//g" | sed -E "s/000(.*)//g"'
+alias eip='curl --silent http://www.whatsmyip.net | grep "Address is" -m 1 | sed -E "s/[^0-9\.]//g" | sed -E "s/.$//" | sed -E "s/^.//"'
+
+alias st="speedtest"
+alias ch="chezmoi"
+
+alias ci="code-insiders"
+alias cin="code-insiders ."
+
+alias ytd-01-list-files="youtube-dl -F "
+alias ytd-simple="youtube-dl --add-metadata -x"
+alias ytd-video="youtube-dl --add-metadata -x"
+alias ytd-best="youtube-dl --audio-format best --embed-thumbnail --add-metadata -x"
+alias ytd-m4a="youtube-dl --audio-format m4a --embed-thumbnail --add-metadata -x"
