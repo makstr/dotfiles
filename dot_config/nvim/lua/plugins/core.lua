a6bf3e4e80d3187389f9657586f5b4b0cddf9fc7
Max Trawinski <max.trawinski@gmail.com>
2023-11-18 19:23:15 -0500

    feat: add lazyvim lua config

---
+++ dot_config/nvim/lua/plugins/core.lua
@@ -0,0 +1,12 @@
+-- local colorscheme = require("lazyvim.plugins.colorscheme")
+
+return {
+  { "projekt0n/github-nvim-theme" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "github_light",
+      colorscheme = "github_dark_dimmed",
+    },
+  },
+}
